bd405466bcd7a274af246d3fce90a681d0fcc5e0
kimetsu-dev <shekeinnugets@gmail.com>
2026-06-23 09:48:09 +0800

    Finale

---
+++ android/app/src/main/java/app/web/ecosort_51471/MainActivity.java
@@ -0,0 +1,5 @@
+package app.web.ecosort_51471;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.web.ecosort_51471',
+  appName: 'Ecosort',
+  webDir: 'build'
+};
+
+export default config;
